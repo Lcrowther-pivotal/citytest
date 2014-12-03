29c4b1063ebc9c92dfcd539b376ac486e3d5475c
Lawrence Crowther <lcrowther@gopivotal.com>
2014-12-03 15:46:22 +0530

    change version number.

--- src/main/java/org/example/cities/VersionController.java
+++ src/main/java/org/example/cities/VersionController.java
@@ -8,6 +8,6 @@ public class VersionController {
 
     @RequestMapping("/version")
     public String index() {
-        return "Welcome to Bangalore";
+        return "Welcome to EMC India";
     }
 }
